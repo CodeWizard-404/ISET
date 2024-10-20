e91a034f112eb16e658790154f2522b69271eb35
Sofien Laghouanem <Sofienlaghouanem@gmail.com>
2024-10-20 18:02:25 +0100

    .

---
+++ 3_DSI/S1/Flutter/Atelier6/app/src/main/java/com/example/atelier6/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.atelier6;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText msha
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edittext);
+    }
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+        String uriString = "geo:0,0?q=" + loc;
+        Uri adressUri = Uri.parse(uriString);
+        Intent intent = new Intent(Intent.ACTION_VIEW, adressUri);
+        startActivity(intent);
+    }
+    public void shareText(View view) {
+        String txt = mWebsiteEditText.getText().toString();
+
+    }
+
+}
