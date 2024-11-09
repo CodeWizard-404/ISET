5d837e2750e2b7ee5f846bb3be64502bb782bc07
Sofien Laghouanem <Sofienlaghouanem@gmail.com>
2024-11-09 12:08:59 +0100

    .

---
+++ 3_DSI/S1/SOA/ProjectManagement2/var/cache/dev/url_matching_routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/soap/affecter-membre' => [
+            [['_route' => 'soap_affecter_membre', '_controller' => 'App\\Controller\\SoapController::soapServer'], null, ['POST' => 0], null, false, false, null],
+            [['_route' => 'affecter_membre', '_controller' => 'App\\Controller\\SoapController::affecterMembre'], null, ['POST' => 0], null, false, false, null],
+        ],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
---
+++ 3_DSI/S1/SOA/ProjectManagement2/vendor/symfony/maker-bundle/src/Resources/skeleton/message/Message.tpl.php
@@ -0,0 +1,16 @@
+<?= "<?php\n" ?>
+
+namespace <?= $namespace; ?>;
+
+final class <?= $class_name."\n" ?>
+{
+    /*
+     * Add whatever properties and methods you need
+     * to hold the data for this message class.
+     */
+
+    // public function __construct(
+    //     public readonly string $name,
+    // ) {
+    // }
+}
