bda133e35eb85cc36f53d50f03b097cff99bf7b3
Sofien Laghouanem <Sofienlaghouanem@gmail.com>
2024-12-01 09:11:08 +0100

    .

--- 3_DSI/S1/SOA/ProjectManagement_WS4/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Create a new Sequelize instance to connect to your database
-const sequelize = new Sequelize('projectmanagement', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
